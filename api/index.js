7857da4b399347cbeffd6a2a587c4a390dfc9186
Joyishh <iaaannnsatrio@gmail.com>
2025-05-14 09:46:04 +0700

    init proyek

---
+++ api/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
