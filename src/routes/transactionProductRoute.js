365ef1781ee503671867b5b14ec120b446617273
Adrian Satrio <115604537+Joyishh@users.noreply.github.com>
2025-06-04 12:19:13 +0700

    wrong commit

--- src/routes/transactionProductRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { topSellingProducts } from '../controllers/transactionsProduct.js';
-
-const router = express.Router();
-
-router.get('/top-selling-products', topSellingProducts);
-
-export default router;
